ea5cc7179f0fbd48de4f5bf45bb1300e063f09fd
abhishek-kumar-16 <kumarabhishekk16dec@gmail.com>
2024-08-20 18:17:55 +0530

    Updated Repo

--- src/main/java/com/project3/mysystem/studentrepo/studentrepo.java
+++ src/main/java/com/project3/mysystem/studentrepo/studentrepo.java
@@ -1,6 +1,10 @@
 package com.project3.mysystem.studentrepo;
 
+import com.project3.mysystem.model.student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface studentrepo extends JpaRepository {
+@Repository
+public interface studentrepo extends JpaRepository<student,Integer> {
+    
 }
