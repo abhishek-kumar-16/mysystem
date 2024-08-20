b5278b05e557414650de8157076d85c13e328ed1
abhishek-kumar-16 <kumarabhishekk16dec@gmail.com>
2024-08-20 17:32:25 +0530

    Completed Model

--- src/main/java/com/project3/mysystem/studentrepo/studentrepo.java
+++ src/main/java/com/project3/mysystem/studentrepo/studentrepo.java
@@ -1,4 +1,6 @@
 package com.project3.mysystem.studentrepo;
 
-public interface studentrepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface studentrepo extends JpaRepository {
 }
